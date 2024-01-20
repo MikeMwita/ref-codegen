36c30188c989fbdc10944f12a0f6131b922b2737
MikeMwita <mwitamike8@gmail.com>
2024-01-21 01:48:38 +0300

    Setup app config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,16 @@
 package config
+
+import "log"
+
+type Config struct {
+}
+
+var config *Config
+
+var Con = func() *Config {
+	if config == nil {
+		log.Fatalln("set up  the  config first")
+	}
+
+	return config
+}
