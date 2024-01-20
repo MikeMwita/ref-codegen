f9401eb4bcfe14b97bcd822ba00c696baaebc70c
MikeMwita <mwitamike8@gmail.com>
2024-01-21 01:53:05 +0300

    Implement ValidateCodes  logic

--- internal/core/usecases/validate_code.go
+++ internal/core/usecases/validate_code.go
@@ -2,35 +2,30 @@ package usecases
 
 import "github.com/MikeMwita/ref-codegen/adapters/input"
 
+type ValidateCodeOutput interface {
+	PresentValidationResult(valid bool)
+}
+
 type ValidateCode struct {
 	codeRepository input.CodeRepository
 }
 
-// Execute executes the use case with the given input and output
-func (v *ValidateCode) Execute(input ValidateCodeInput, output ValidateCodeOutput) error {
-	// get the Code entity from the repository by the value
-	code, err := v.codeRepository.GetByValue(input.Value)
-	if err != nil {
-		return err
-	}
-	// validate the Code entity
-	valid, err := code.Validate()
-	if err != nil {
-		return err
-	}
-	// present the validation result to the output
-	output.Present(valid)
-	return nil
-}
-
-// ValidateCodeInput is a type that defines the input for the ValidateCode use case
 type ValidateCodeInput struct {
 	Value string // the value of the code or key
 }
 
-// ValidateCodeOutput is an interface that defines the output for the ValidateCode use case
-type ValidateCodeOutput interface {
-	Present(valid bool) // presents the validation result to the output
+func (v *ValidateCode) Execute(input ValidateCodeInput, output ValidateCodeOutput) error {
+	code, err := v.codeRepository.GetByValue(input.Value)
+	if err != nil {
+		return err
+	}
+	valid, err := code.Validate()
+	if err != nil {
+		return err
+	}
+
+	output.PresentValidationResult(valid)
+	return nil
 }
 
 func NewValidateCode(codeRepository input.CodeRepository) *ValidateCode {
